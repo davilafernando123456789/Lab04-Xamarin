aca1303687697885056ed13e48756a4b1bb7a03a
Alumno <Alumno@LAB1509-16>
2023-09-29 17:04:07 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio02/Ejercicio02/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Ejercicio02
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
+                                              
